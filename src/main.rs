c1674d0a40ed1a0d95e4bdafeba23ae48524593d
Tom Parker-Shemilt <palfrey@tevp.net>
2021-10-28 23:57:35 +0100

    Add non-working TLS

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod server;
-use tonic::{transport::Server};
+use tonic::{transport::{Server, ServerTlsConfig}};
 use server::tf::provider_server::ProviderServer;
 use futures::{TryFutureExt, try_join};
 use anyhow::{anyhow,Result};
@@ -11,7 +11,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "0.0.0.0:10000".parse()?;
     let hello_world = server::HelloWorldProvider::default();
 
+    let tls_config = ServerTlsConfig::new();
+
     let serve = Server::builder()
+        .tls_config(tls_config).unwrap()
         .add_service(ProviderServer::new(hello_world))
         .serve(addr);
 
