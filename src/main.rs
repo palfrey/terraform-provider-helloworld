f6a03c1aaaf6db6324d6c8579cd560e4706c4770
Tom Parker-Shemilt <palfrey@tevp.net>
2021-10-30 19:10:51 +0100

    Working mTLS setup

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 mod server;
-use tonic::{transport::{Server, ServerTlsConfig}};
+use std::env;
+
+use anyhow::{anyhow, Result};
+use futures::{try_join, TryFutureExt};
+use rcgen::{BasicConstraints, IsCa};
 use server::tf::provider_server::ProviderServer;
-use futures::{TryFutureExt, try_join};
-use anyhow::{anyhow,Result};
+use tonic::transport::{Certificate, Identity, Server, ServerTlsConfig};
 
 const CORE_PROTOCOL_VERSION: u8 = 1;
 
@@ -11,25 +14,43 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "0.0.0.0:10000".parse()?;
     let hello_world = server::HelloWorldProvider::default();
 
-    let tls_config = ServerTlsConfig::new();
+    let client_cert = Certificate::from_pem(
+        env::var("PLUGIN_CLIENT_CERT").expect("Expected to find cert in PLUGIN_CLIENT_CERT"),
+    );
+    let mut cp = rcgen::CertificateParams::new(vec!["localhost".to_string()]);
+    cp.is_ca = IsCa::Ca(BasicConstraints::Unconstrained);
+    let server_cert = rcgen::Certificate::from_params(cp)?;
+    let identity = Identity::from_pem(
+        server_cert.serialize_pem()?,
+        server_cert.serialize_private_key_pem(),
+    );
+    let mut tls_config = ServerTlsConfig::new();
+    tls_config = tls_config.client_ca_root(client_cert);
+    tls_config = tls_config.identity(identity);
 
     let serve = Server::builder()
-        .tls_config(tls_config).unwrap()
+        .tls_config(tls_config)
+        .unwrap()
         .add_service(ProviderServer::new(hello_world))
         .serve(addr);
 
-    async fn info() -> Result<()> {
-        println!("{}|{}|{}|{}|{}|{}",
-        CORE_PROTOCOL_VERSION,
-        "5",
-        "tcp",
-        "localhost:10000",
-        "grpc",
-        "cert");
+    async fn info(server_cert: rcgen::Certificate) -> Result<()> {
+        println!(
+            "{}|{}|{}|{}|{}|{}",
+            CORE_PROTOCOL_VERSION,
+            "5",
+            "tcp",
+            "localhost:10000",
+            "grpc",
+            base64::encode_config(
+                server_cert.serialize_der().unwrap(),
+                base64::STANDARD_NO_PAD
+            )
+        );
         Ok(())
     }
 
-    try_join!(serve.map_err(|e| anyhow!(e)), info())?;
+    try_join!(serve.map_err(|e| anyhow!(e)), info(server_cert))?;
 
     Ok(())
 }
