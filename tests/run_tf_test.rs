bafd87f43086baa8f26037d0f70eb5974a553404
Tom Parker-Shemilt <palfrey@tevp.net>
2021-10-30 19:11:03 +0100

    Log all things Terraform

--- tests/run_tf_test.rs
+++ tests/run_tf_test.rs
@@ -4,6 +4,7 @@ use std::{env, process::Command};
 fn run_tf() {
     let path = env::current_dir().unwrap();
     let test_path = path.join("tf");
+    env::set_var("TF_LOG", "debug");
     env::set_current_dir(test_path).unwrap();
     let status = Command::new("../terraform").arg("plan").status().unwrap();
     assert!(status.success());
