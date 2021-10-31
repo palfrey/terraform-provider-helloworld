91e6129a1becaa425d550d4f78609f653d1fd29f
Tom Parker-Shemilt <palfrey@tevp.net>
2021-10-31 13:16:40 +0000

    Fix tfplugin path

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure().compile(
         &[
-            "external/terraform/docs/plugin-protocol/tfplugin6.0.proto",
+            "external/terraform/docs/plugin-protocol/tfplugin5.0.proto",
             "external/go-plugin/internal/plugin/grpc_stdio.proto",
         ],
         &[
