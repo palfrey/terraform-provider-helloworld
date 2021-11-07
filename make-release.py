c2c97545c89bbf1afec7c481b67490e1ad559bbf
Tom Parker-Shemilt <palfrey@tevp.net>
2021-11-07 16:19:26 +0000

    Fix paths in release creator

--- make-release.py
+++ make-release.py
@@ -7,8 +7,9 @@ if len(sys.argv) != 2:
 
 version = sys.argv[1]
 
-shutil.copy("target/debug/terraform-hello-world", f"target/terraform-provider-hello-world_v{version}")
-subprocess.check_call(["zip", f"terraform-provider-hello-world_{version}_linux_amd64.zip", f"terraform-provider-hello-world_v{version}"], cwd="target")
-shas = subprocess.check_output(["shasum", "-a", "256", f"terraform-provider-hello-world_{version}_linux_amd64.zip"], cwd="target").decode('utf-8')
-open(f"target/terraform-provider-hello-world_{version}_SHA256SUMS", "w").write(shas)
-subprocess.check_output(["gpg", "--detach-sign", f"terraform-provider-hello-world_{version}_SHA256SUMS"], cwd="target")
+subprocess.check_call(["strip", "target/debug/terraform-provider-helloworld"])
+shutil.copy("target/debug/terraform-provider-helloworld", f"target/terraform-provider-helloworld_v{version}")
+subprocess.check_call(["zip", f"terraform-provider-helloworld_{version}_linux_amd64.zip", f"terraform-provider-helloworld_v{version}"], cwd="target")
+shas = subprocess.check_output(["shasum", "-a", "256", f"terraform-provider-helloworld_{version}_linux_amd64.zip"], cwd="target").decode('utf-8')
+open(f"target/terraform-provider-helloworld_{version}_SHA256SUMS", "w").write(shas)
+subprocess.check_output(["gpg", "--detach-sign", f"terraform-provider-helloworld_{version}_SHA256SUMS"], cwd="target")
